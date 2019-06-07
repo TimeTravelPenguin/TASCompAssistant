3ccb6a2eb33ac93f76a83ebd988ec221d54db8c8
Phillip <Filuperwinsmith@gmail.com>
2019-06-07 19:25:42 +1000

    Initial commit

---
+++ Competitor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASCompAssistant
+{
+	internal class Competitor
+	{
+		public int Place { get; set; }
+		public string Username { get; set; }
+		public int VIStart { get; set; }
+		public int VIEnd { get; set; }
+		public int VIs { get { return VIEnd - VIStart; } }
+		public double TimeInSeconds { get { return GetTime(); } }
+		public string TimeFormated { get { return GetFormatTime(); } }
+		public int Rerecords { get; set; }
+		public bool DQ { get; set; }
+		public string DQReason { get; set; }
+		public double Score { get; set; }
+
+		private double GetTime()
+		{
+			return Math.Round((double)VIs / 60, 3);
+		}
+
+		private string GetFormatTime()
+		{
+			string time = string.Empty;
+
+			double sec = TimeInSeconds;
+			double ms = Math.Round(sec - Math.Floor(sec), 3);
+			var min = Math.Floor(sec) % 60;
+
+			if (sec < 60)
+			{
+				time = $"{Math.Floor(sec)}\'{ms.ToString().TrimStart("0.".ToCharArray())}";
+			}
+			else if (sec >= 60)
+			{
+				time = $"{min}\"{sec}\'{ms}";
+			}
+
+			return time;
+		}
+	}
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TASCompAssistant
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ObservableCollection<Competitor> Competitors = new ObservableCollection<Competitor>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MenuItem_File_Exit_Click(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		private void btn_AddCompetitor_Click(object sender, RoutedEventArgs e)
+		{
+			/*	TODO:
+					- Error handle & chack that textboxes contain numbers ONLY
+					- Calculate Time & TimeFormated
+					- Convert VIs to formatted time!!! ^
+					- Add DQ Reasons
+			*/
+
+			var competitor = new Competitor()
+			{
+				Username = txtbox_Username.Text,
+				VIStart = Convert.ToInt32(txtbox_VIStart.Text),
+				VIEnd = Convert.ToInt32(txtbox_VIEnd.Text),
+				Rerecords = Convert.ToInt32(txtbox_Rerecords.Text),
+				DQ = chkbox_DQ.IsChecked.Value,
+				DQReason = "coming soon...", // Fix this later
+				Score = 0
+			};
+
+			// Calculate Place
+			competitor.Place = 0;
+
+			Competitors.Add(competitor);
+
+			//UpdateCompetitionLeaderboard();
+		}
+
+		private void UpdateCompetitionLeaderboard()
+		{
+			Datagrid_Competition.Items.Clear();
+			foreach (var competitor in Competitors)
+			{
+				Datagrid_Competition.Items.Add(competitor);
+			}
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			Datagrid_Competition.ItemsSource = Competitors;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			string msg = string.Empty;
+
+			foreach (var item in Competitors)
+			{
+				msg += $"{item.Username}   DQ: {item.DQ}" + Environment.NewLine;
+			}
+
+			MessageBox.Show(msg);
+		}
+	}
+}
