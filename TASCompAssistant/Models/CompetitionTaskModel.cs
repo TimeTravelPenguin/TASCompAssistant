92bf2a1256b913751763750889deeca5ac8715e4
Phillip <Filuperwinsmith@gmail.com>
2019-07-06 22:58:47 +1000

    Changed words on UI to be more general

--- TASCompAssistant/Models/CompetitionTaskModel.cs
+++ TASCompAssistant/Models/CompetitionTaskModel.cs
@@ -46,7 +46,7 @@ namespace TASCompAssistant.Models
             get
             {
                 // string tip = $"Name: {CompetitionName}\nStart: {DueDates.StartDate, 0:D}\nEnd: {DueDates.EndDate, 0:D}\nDue Time: {DueDates.DueTime, 0:hh:mm tt}";
-                string tip = $"Description: {Metadata.TaskDescription}\nRule Count: {Metadata.Rules.Count}";
+                string tip = $"Description:\n{Metadata.TaskDescription}\n\nNumber of Rules: {Metadata.Rules.Count}";
                 return tip;
             }
         }
